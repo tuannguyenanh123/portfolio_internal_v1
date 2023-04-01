c332c1790655452db4f3aaef79c0f68d7e204224
tuanna <anhtuan120600ohayo@gmail.com>
2023-04-01 15:37:56 +0700

    fcm

---
+++ constants.js
@@ -0,0 +1,178 @@
+export const METADATA = {
+  title: "Portfolio | Nguyen Anh Tuan",
+  description:
+    "Nguyen Anh Tuan is a Software Engineer from Viet Nam, interested in crafting beautiful and functional applications. Tuan is pursuing his undergraduate from The University of Danang - University of Science and Technology, Da Nang.",
+  siteUrl: "",
+  twitterHandle: "@tuananhnguyen",
+};
+
+export const MENULINKS = [
+  {
+    name: "Home",
+    ref: "home",
+  },
+  {
+    name: "Skills",
+    ref: "skills",
+  },
+  {
+    name: "Projects",
+    ref: "projects",
+  },
+  {
+    name: "Work",
+    ref: "work",
+  },
+  {
+    name: "Contact",
+    ref: "contact",
+  },
+];
+
+export const TYPED_STRINGS = [
+  "A Software Engineer",
+  "I build things for the web",
+  "I create aesthetic and modern apps",
+];
+
+export const SOCIAL_LINKS = [
+  {
+    name: "mail",
+    url: "mailto: anhtuan120600ohayo@gmail.com",
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/anh-tuan-nguyen-6a5876155/",
+  },
+  {
+    name: "github",
+    url: "https://github.com/tuannguyenanh123",
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/anhtuan.g_/",
+  },
+  {
+    name: "twitter",
+    url: "",
+  },
+];
+
+export const SKILLS = {
+  languagesAndTools: [
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "sass",
+    "c++",
+    "firebase",
+    "moralis",
+    "stripe",
+    // "figma",
+    "matlab",
+  ],
+  librariesAndFrameworks: [
+    "react",
+    "redux",
+    "nextjs",
+    "tailwindcss",
+    "styledcomponents",
+    "antdesign",
+    "mui",
+  ],
+  databases: [
+    "mysql",
+    "mongodb",
+  ],
+  other: ["nodejs", "git", "sanity-io", "nestjs"],
+};
+
+export const PROJECTS = [
+  {
+    name: "NTERNAL TOOL",
+    image: "/projects/project1.png",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description: "Tuan UI clone using NextJS + Tailwind CSS + Mui 🛏️",
+    gradient: ["#F14658", "#DC2537"],
+    url: "http://internal-dev.dipro-tech.com/signin",
+    tech: ["react", "nextjs", "tailwindcss", "mui"],
+  },
+  // {
+  //   name: "Shotime",
+  //   image: "",
+  //   blurImage: "",
+  //   description:
+  //     "Developed with complete E-commerce functionality and User Authentication 🛍️",
+  //   gradient: ["#FFCF1B", "#FF881B"],
+  //   url: "https://shubh73-shotime.vercel.app/",
+  //   tech: ["react", "tailwindcss", "redux", "mongodb", "stripe"],
+  // },
+  {
+    name: "CLOUD MEDIA",
+    image: "/projects/project2.png",
+    blurImage: "/projects/blur/medium-blur.webp",
+    description: "Medium UI clone using NextJS + Tailwind CSS ✍🏻",
+    gradient: ["#FFA62E", "#EA4D2C"],
+    url: "https://cloud-media-candydev.herokuapp.com",
+    tech: ["javascript", "react", "mui", "tailwindcss", "nodejs", "mongodb"],
+  },
+  {
+    name: "Inshorts",
+    image: "/projects/inshorts.webp",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description:
+    "Conversational Voice Controlled React News Application using Alan AI 🎙",
+    gradient: ["#000066", "#6699FF"],
+    url: "",
+    tech: ["react", "chakra-ui", "alan"],
+  },
+  {
+    name: "Tesla",
+    image: "/projects/tesla.webp",
+    blurImage: "/projects/blur/tesla-blur.webp",
+    description: "A Tesla React Native App 🏎️",
+    gradient: ["#142D46", "#2E4964"],
+    url: "",
+    tech: ["react"],
+  },
+];
+
+export const WORK = [
+  {
+    company: "VNPT IT",
+    title: "Software Engineer Intern",
+    location: "Tam Ki, Quang Nam",
+    range: "June - July 2022",
+    responsibilities: [
+      "Developed app integration with REST APIs, Google Maps, User Auth, Stripe and other libraries.",
+      "Implemented CRUD features for all the services and providers.",
+    ],
+    url: "http://quangnam.vnpt.vn/",
+  },
+  {
+    company: "Dipro - Leading in providing Digital Transformation Solutions",
+    title: "Dipro DX",
+    location: "Da Nang",
+    range: "August 2022 - Present",
+    responsibilities: [
+      "Developed app integration with REST APIs, Google Maps, User Auth, Stripe and other libraries.",
+      "Implemented CRUD features for all the services and providers",
+    ],
+    url: "https://www.facebook.com/diprovnjp/",
+  },
+  // {
+  //   company: "Spacenos",
+  //   title: "Web Developer Intern",
+  //   location: "Bangalore, Karnataka",
+  //   range: "September - December 2021",
+  //   responsibilities: [
+  //     "Led the Full Stack revamp on the Admin Portal.",
+  //     "Developed app integration with REST APIs, Google Maps, User Auth, Stripe and other libraries.",
+  //     "Implemented CRUD features for all the services and providers.",
+  //   ],
+  //   url: "https://spacenos.com/",
+  // },
+];
+
+export const GTAG = "G-5HCTL2TJ5W";
